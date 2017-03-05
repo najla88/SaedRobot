22d74ccdfd3e63f66ae97643036aae18fd07f778
najla88 <najlaalghofaily@gmail.com>
2017-03-05 13:04:55 +0300

    adding login error message

--- login.py
+++ login.py
@@ -11,21 +11,34 @@ class login():
 		self.builder.add_from_file("Loin.glade")
 		window = self.builder.get_object("window1")
 		usernameBtn=self.builder.get_object("loginBtn")
+		forgotPassBtn=self.builder.get_object("forgotBtn")
 		usernameBtn.connect("clicked",self.login)
+		forgotPassBtn.mousePressEvent = self.forgot
+
 		window.show()
 
 	def login(self,button):
 		
 		username = self.builder.get_object("username")
 		password = self.builder.get_object("password")
+		loginError = self.builder.get_object("loginError")
+		loginError.connect("activate-current-link",self.forgot)
+
 		db = sqlite3.connect('testDB.db')
 		c = db.cursor()
 		c.execute('SELECT * from users WHERE username= ? AND password= ?' , (str(username.get_text()), str(password.get_text())))
 		data=c.fetchall()
 		if len(data)>0:
 			print "Welcome"
+			loginError.set_text('')
+
 		else:
-			print "Login failed"
+			loginError.set_text('Wronge username or password')
+			
+	def forgot(self,event):
+		
+		window2 = self.builder.get_object("window2")
+		window2.show()			
 			
 window=login()
 Gtk.main()
