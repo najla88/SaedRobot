43f677544dad764bae527734f0098fa2826e37e3
Maryam Ahmed <m.aa.ksa@hotmail.com>
2017-03-10 16:03:30 +0300

    pushing updated maryam

--- maryam.py
+++ maryam.py
@@ -5,6 +5,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 import login
+import changePass
 
 class userHome():
 	
@@ -25,15 +26,70 @@ class userHome():
 
 	def createTask(self,button):
 		
-		
 		self.window.destroy()
-		self.window =create_task()
-		##we should have create task class
+		self.window =tapeInfo('C00001')
+		##we should have scane class
 			
 	def changePassword(self,button):
 		self.window.destroy()
-		window2 = login.change_password()
+		window2 = changePass.change_password()
+		
+class tapeInfo():
+	
+	builder =None
+	window = None
+	projectName = None
+	tapeName = None
+	rackName = None
+	slotNumber = None
+	
+	def __init__(self,a):
+		self.builder = Gtk.Builder()
+		self.builder.add_from_file("HomeUser.glade")
+		self.window = self.builder.get_object("window2")
+		proceedBtn=self.builder.get_object("proceedBtn")
+		cancelBtn=self.builder.get_object("cancelBtn")
+		backBtn=self.builder.get_object("backBtn")
+		self.projectName=self.builder.get_object("projectName")
+		self.tapeName=self.builder.get_object("tapeName")
+		self.rackName=self.builder.get_object("rackName")
+		self.slotNumber=self.builder.get_object("slotNumber")
+		proceedBtn.connect("clicked",self.back)
+		cancelBtn.connect("clicked",self.back)
+		backBtn.connect("clicked",self.back)
 
+		db = sqlite3.connect('SaedRobot.db')
+		c = db.cursor()
+		c.execute('SELECT * from inventory WHERE volser= ?' , (a,))
+		data=c.fetchone()
+		
+		if data !=None and len(data)>0:
+			self.projectName.set_text(data[1])
+			self.tapeName.set_text(data[0])
+			self.rackName.set_text(data[2])
+			self.slotNumber.set_text(str(data[3]))
 
-#window=userHome()
+		else:
+			#loginError.set_text('Invalid username or password, please try again')
+			## this sould be in the scan page class
+			dialog=Gtk.MessageDialog(None,0,Gtk.MessageType.ERROR,Gtk.ButtonsType.OK,"Could not detect the barcode, please scane again")
+			dialog.run()
+			dialog.close()
+			
+
+		self.window.show()
+
+	def back(self,button):
+		self.window.destroy()
+		self.window=userHome()
+	#def proceed(self,button):
+		#self.window.destroy()
+		#self.window=scaneMore()
+    #def cancel(self,button):
+		#self.window.destroy()
+		#self.window=scaneTape()
+
+		
+# put thim coment chang the window in the first class to window1
+#window=create_task()
 #Gtk.main()
